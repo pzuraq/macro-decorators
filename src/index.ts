f02c24db1d2cc817a7fe800a6428c5bb92cb1778
Pavol Dano <pavol.dano@zonky.cz>
2020-02-23 20:44:14 +0100

    Export utility functions

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { getPath, getPaths, setPath } from './utils';
+
 /**
  * ```typescript
  * import { MacroGetter } from 'macro-decorators';
@@ -146,38 +148,6 @@ export default function macro(definition: MacroGetter | MacroDescriptor): Proper
 
     return desc;
   };
-}
-
-function getPath(obj: any, path: string) {
-  let segments = path.split('.');
-  let current = obj;
-
-  for (let segment of segments) {
-    if (current === undefined || current === null) {
-      break;
-    }
-
-    current = typeof current.get === 'function' ? current.get(segment) : current[segment];
-  }
-
-  return current;
-}
-
-function getPaths(obj: any, paths: string[]) {
-  return paths.map(p => getPath(obj, p));
-}
-
-function setPath(obj: any, path: string, value: any) {
-  let objPath = path.substr(0, path.lastIndexOf('.'));
-  let key = path.substr(path.lastIndexOf('.') + 1);
-
-  let resolvedObj = objPath ? getPath(obj, objPath) : obj;
-
-  if (typeof resolvedObj.set === 'function') {
-    resolvedObj.set(key, value);
-  } else {
-    resolvedObj[key] = value;
-  }
 }
 
 // **** Aliasing ****
